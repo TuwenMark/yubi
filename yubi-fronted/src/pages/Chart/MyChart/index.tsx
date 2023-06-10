90360bd5123deb4d03772a1a6ede70bf72f1fd7f
WinterTin <2537201614@qq.com>
2023-06-10 08:45:50 +0800

    开发图表管理

--- yubi-fronted/src/pages/Chart/MyChart/index.tsx
+++ yubi-fronted/src/pages/Chart/MyChart/index.tsx
@@ -1,11 +1,17 @@
 import {listMyChartByPageUsingPOST} from '@/services/yubi/chartController';
-import {message,} from 'antd';
+import {Avatar, Card, List, message, Skeleton,} from 'antd';
 import React, {useEffect, useState} from 'react';
+import ReactECharts from "echarts-for-react";
+import {useModel} from "@umijs/max";
+import Search from "antd/es/input/Search";
 
 const MyChart: React.FC = () => {
+  const {initialState} = useModel('@@initialState');
+  const {currentUser} = initialState ?? {};
   const [loading, setLoading] = useState<boolean>(false);
   const initialParams = {
     pageSize: 5,
+    current: 1,
   }
   const [searchParams, setSearchParams] = useState<API.ChartQueryRequest>({...initialParams});
   const [chartList, setChartList] = useState<API.Chart[]>([])
@@ -16,9 +22,15 @@ const MyChart: React.FC = () => {
     try {
       const res = await listMyChartByPageUsingPOST(searchParams);
       if (res?.data?.records) {
+        res?.data?.records.forEach(data => {
+          const chartOption = JSON.parse(data.genChart??'');
+          chartOption.title = undefined;
+          data.genChart = JSON.stringify(chartOption)
+        })
+        console.log(res.data.records[0])
         setChartList(res.data.records);
         setTotal(res.data.total ?? 0)
-      }else {
+      } else {
         message.error("获取图表失败，无图表信息");
       }
     } catch (e) {
@@ -32,12 +44,55 @@ const MyChart: React.FC = () => {
   }, [searchParams])
 
   return (
-    <div>
-      数据列表：
-      {JSON.stringify(chartList)}
-      <br/>
-      总数：{total}
-    </div>
+      <Skeleton loading={loading} active={true}>
+        <Search placeholder="请输入图表名称" loading={loading} onSearch={(value) => {
+          // 带上初始搜索条件，这样才会到第一页
+          setSearchParams({
+            ...initialParams,
+            name: value
+          })
+        }} />
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 1,
+            md: 1,
+            lg: 2,
+            xl: 2,
+            xxl: 2,
+          }}
+          pagination={{
+            onChange: (page, pageSize) => {
+              setSearchParams({
+                ...searchParams,
+                current: page,
+                pageSize: pageSize
+              })
+            },
+            pageSize: 4,
+            current: searchParams.current,
+            total: total
+          }}
+          dataSource={chartList}
+          renderItem={(item) => (
+            <Card>
+              <List.Item
+                key={item.id}
+              >
+                <List.Item.Meta
+                  avatar={<Avatar src={currentUser?.userAvatar} />}
+                  title={<div>{item.name}</div>}
+                  description={<div><div className="margin-15">图表类型：{item.chartType}</div><div>分析结论：{item.genResult}</div></div>}
+                />
+                <div>
+                  <ReactECharts option={JSON.parse(item.genChart ?? '')}/>
+                </div>
+              </List.Item>
+            </Card>
+          )}
+        />
+      </Skeleton>
   );
 };
 export default MyChart;
